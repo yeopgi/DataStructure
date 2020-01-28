e51b14df615a9e60ea12cd28f640adaf416567be
yeopgi <7677942@hanmail.net>
2020-01-28 10:52:26 +0900

    KnuthMOrrisPratt

---
+++ c_c++_project/KnuthMorrisPratt/CMakeFiles/KMP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KMP.dir/main.c.o"
+  "CMakeFiles/KMP.dir/KnuthMorrisPratt.c.o"
+  "KMP.pdb"
+  "KMP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/KMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
